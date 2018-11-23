ca2a1f112050c942b9749e8a62915b032ff49e3c
Dirk Kalmbach <dirk.kalmbach@me.com>
2018-11-23 12:31:40 -0800

    with Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,6 +1,10 @@
 import feedparser
 from flask import Flask
 
+# the magic which takes a Jinja template as input and produces pure HTML:
+from flask import render_template 
+
+
 app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -15,18 +19,17 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 # def cnn():
 #    return get_news('cnn')
 def get_news(publication="bbc"):
- feed = feedparser.parse(RSS_FEEDS[publication])
- first_article = feed['entries'][0]
- return """<html>
-   <body>
-       <h1>Headlines </h1>
-       <h2>This is [cannot find the feed VAR] news feed</h2>
-       <b>{0}</b> </ br>
-       <i>{1}</i> </ br>
-       <p>{2}</p> </ br>
-   </body>
-</html>""".format(first_article.get("title"), first_article.
-get("published"), first_article.get("summary"))
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    # first_article = feed['entries'][0]
+    #return render_template("home.html", article=first_article)
+    return render_template("home.html", articles=feed['entries'])
+
+                            # title=first_article.get("title"),
+                            # published=first_article.get("published"),
+                            # summary=first_article.get("summary")
+                            # )
+
+
 if __name__ == "__main__":
  app.run(port=5000, debug=True)
 
