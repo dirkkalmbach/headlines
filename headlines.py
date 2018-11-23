d673bfa925ea864011f18e5ff7e6b3bb187b2d4a
Dirk Kalmbach <dirk.kalmbach@me.com>
2018-11-23 12:59:30 -0800

    Using GET

--- headlines.py
+++ headlines.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 # the magic which takes a Jinja template as input and produces pure HTML:
 from flask import render_template 
+from flask import request
 
 
 app = Flask(__name__)
@@ -10,26 +11,21 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
             'fox': 'http://feeds.foxnews.com/foxnews/latest',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
 @app.route("/<publication>") # Variabel statt hard coded URL
 
-# def bbc():
-#    return get_news('bbc')
-# @app.route("/cnn")
-# def cnn():
-#    return get_news('cnn')
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+           publication = "bbc"
+    else:
+           publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    # first_article = feed['entries'][0]
-    #return render_template("home.html", article=first_article)
     return render_template("home.html", articles=feed['entries'])
 
-                            # title=first_article.get("title"),
-                            # published=first_article.get("published"),
-                            # summary=first_article.get("summary")
-                            # )
 
 
 if __name__ == "__main__":
- app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
 
